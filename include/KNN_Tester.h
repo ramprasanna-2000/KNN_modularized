f1ba1a4a7c2fe320ca6fe87cd7043533d6451807
Jacinto Mendes <jmz093@gmail.com>
2014-03-12 20:44:07 +0530

    made completely dynamic

--- include/KNN_Tester.h
+++ include/KNN_Tester.h
@@ -23,46 +23,45 @@
 using namespace std;
 using namespace cv;
 
-#define TOTAL_CLASSES 2
-
 class KNN_Tester {
 
 private:
 
-	Mat _samples;			// For storing samples
-	Mat _labels;			// For storing labels
-	KNearest knn;
+    Mat _samples;			// For storing samples
+    Mat _labels;			// For storing labels
+    KNearest knn;
 
-	int TOTAL_SAMPLES;
+    int TOTAL_SAMPLES;
+    int TOTAL_CLASSES;
 
-	// Test the KNN classifier
-	void test_classifier( string  PATH_TO_TESTING_DATA );
+    // Test the KNN classifier
+    void test_classifier( string  PATH_TO_TESTING_DATA);
 
-	// Serialize (save) the training to file.
-	void deserialize_training();
+    // Serialize (save) the training to file.
+    void deserialize_training();
 
 public:
 
-	KNN_Tester(string PATH_TO_TESTING_DATA, int total_samples) : TOTAL_SAMPLES(total_samples) {
+    KNN_Tester(string PATH_TO_TESTING_DATA, int total_samples, int total_classes) : TOTAL_SAMPLES(total_samples), TOTAL_CLASSES(total_classes) {
 
         deserialize_training();
 
-		test_classifier(PATH_TO_TESTING_DATA );
+        test_classifier(PATH_TO_TESTING_DATA);
 
-		cvWaitKey(0);
-	}
+        cvWaitKey(0);
+    }
 
     // Test if class works
-	string sayHello() {
-		return "hello";
-	}
+    string sayHello() {
+        return "hello";
+    }
 
-	// Convert (input) integer to String (using stringstream)
-	string convertIntToString(int number) {
-		stringstream ss;			//create a stringstream
-		ss << number;				//add number to the stream
-		return ss.str();			//return a string with the contents of the stream
-	}
+    // Convert (input) integer to String (using stringstream)
+    string convertIntToString(int number) {
+        stringstream ss;			//create a stringstream
+        ss << number;				//add number to the stream
+        return ss.str();			//return a string with the contents of the stream
+    }
 
 };
 
@@ -78,7 +77,7 @@ void KNN_Tester::deserialize_training() {
 
     // Read stored sample and label for training
 
-    FileStorage Data("TrainingData.yml",FileStorage::READ); // Read traing data to a Mat
+    FileStorage Data("TrainingData.yml",FileStorage::READ); // Read training data to a Mat
     Data["data"] >> _samples;
     Data.release();
 
@@ -118,52 +117,50 @@ void KNN_Tester::deserialize_training() {
 // Test the KNN classifier
 void KNN_Tester::test_classifier( string string_to_training_data) {
 
-	// Mat for storing a single sample
-	Mat sample;
+    // Mat for storing a single sample
+    Mat sample;
 
     // Mat for storing CV_32FC1
-	Mat sample_flattened;
+    Mat sample_flattened;
 
-	// Change directory to training data directory
-	chdir( string_to_training_data.c_str() );
+    // Change directory to training data directory
+    chdir( string_to_training_data.c_str() );
 
     char stackBuffer[512];
 
-    //testing stuff
-        //count of correct classifications
-        int correct_class[TOTAL_CLASSES] = {};
+    //count of correct classifications
+    int* correct_class = new int[TOTAL_CLASSES] {};
 
-        //count of wrong classifications
-        int wrong_class[TOTAL_CLASSES] = {};
-    //testing stuff
+    //count of wrong classifications
+    int* wrong_class = new int[TOTAL_CLASSES] {};
 
-	for(int CURR_CLASS=0; CURR_CLASS<TOTAL_CLASSES; CURR_CLASS++) {
+    for(int CURR_CLASS=0; CURR_CLASS<TOTAL_CLASSES; CURR_CLASS++) {
 
-		for(int CURR_SAMPLE=0; CURR_SAMPLE<TOTAL_SAMPLES; CURR_SAMPLE++) {
+        for(int CURR_SAMPLE=0; CURR_SAMPLE<TOTAL_SAMPLES; CURR_SAMPLE++) {
 
             chdir( (convertIntToString(CURR_CLASS)).c_str());
 
 
-			string filename = "IMG ("+ convertIntToString(CURR_SAMPLE+1) + ").png";
+            string filename = "IMG ("+ convertIntToString(CURR_SAMPLE+1) + ").png";
 #if DEBUG
             cout << "current dir is "  << getcwd(stackBuffer, sizeof(stackBuffer)) << endl;
-			cout << filename << endl;
+            cout << filename << endl;
 #endif // DEBUG
 
-			// Read each sample
-			sample = imread(filename, 0);
+            // Read each sample
+            sample = imread(filename, 0);
             //threshold(sample,sample,200,255,THRESH_BINARY_INV);
             cv::threshold(sample, sample, 0, 255, CV_THRESH_BINARY_INV | CV_THRESH_OTSU);
 
             /* TODO TRY CATCH FOR -> IF NOT ABLE TO READ FILE THEN THROW EXCEPTION - HALT*/
 
-			// Resize sample to 32,32
-			resize(sample, sample, Size(32,32), 0, 0, INTER_CUBIC );
+            // Resize sample to 32,32
+            resize(sample, sample, Size(32,32), 0, 0, INTER_CUBIC );
 
-			// Convert to float - flatten the multidim to single dim (single chan)
-			sample.convertTo(sample_flattened, CV_32FC1);
+            // Convert to float - flatten the multidim to single dim (single chan)
+            sample.convertTo(sample_flattened, CV_32FC1);
 
-			float p=knn.find_nearest(sample_flattened.reshape(1,1), 1);
+            float p=knn.find_nearest(sample_flattened.reshape(1,1), 1);
 
 
             // if sample is NOT classified correctly
@@ -175,14 +172,14 @@ void KNN_Tester::test_classifier( string string_to_training_data) {
                 correct_class[CURR_CLASS]++;
             }
 
-		}
+        }
 
         // Go up 1 file directory
-		chdir("../");
-	}
+        chdir("../");
+    }
 
     // Go up 2 file directories
-	chdir("../..");
+    chdir("../..");
 
     //testing %
     for(int CURR_CLASS=0; CURR_CLASS<TOTAL_CLASSES; CURR_CLASS++) {
@@ -190,6 +187,9 @@ void KNN_Tester::test_classifier( string string_to_training_data) {
         cout <<  correct_class[CURR_CLASS]*100/TOTAL_SAMPLES << "%" <<  endl;
 
     }
+
+    delete[] correct_class;
+    delete[] wrong_class;
 }
 
 #endif // KNN_TESTER_H
